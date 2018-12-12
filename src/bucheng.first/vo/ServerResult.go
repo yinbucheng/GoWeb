cd73cae174673bb1bfe97cc3f5bb648bdc5d556a
yinchong <yin.chong@intellif.com>
2018-12-12 15:43:57 +0800

    提交新包

--- src/bucheng.first/vo/ServerResult.go
+++ src/bucheng.first/vo/ServerResult.go
@@ -2,6 +2,9 @@ package vo
 
 import "github.com/gin-gonic/gin"
 
+/**
+快速成功
+*/
 func SuccessServerResult(data interface{}, c *gin.Context) {
 	c.JSON(200, gin.H{
 		"code":    200,
@@ -10,6 +13,9 @@ func SuccessServerResult(data interface{}, c *gin.Context) {
 	})
 }
 
+/**
+快速失败
+*/
 func FailServerResult(message string, c *gin.Context) {
 	c.JSON(200, gin.H{
 		"code":    500,
